ac612aef8310c8173d75211f77e39fd9f58c2949
afliw@VM-VsCS <aflivv@gmail.com>
2016-02-17 02:06:31 -0300

    Nueva clase para facilitar el manejo de los DataGridView

---
+++ FerreteriaSL/Clases Genericas/DGVHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+using System.Windows.Forms;
+
+namespace FerreteriaSL.Clases_Genericas
+{
+    static class DgvHelper
+    {
+        public static DataTable ToDataTable(DataGridView input)
+        {
+            DataTable output = new DataTable();
+            foreach (DataGridViewColumn col in input.Columns)
+            {
+                output.Columns.Add(col.HeaderText);
+            }
+
+            foreach (DataGridViewRow row in input.Rows)
+            {
+                DataRow dRow = output.NewRow();
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    dRow[cell.ColumnIndex] = cell.Value;
+                }
+                output.Rows.Add(dRow);
+            }
+            return output;
+        }
+    }
+}
